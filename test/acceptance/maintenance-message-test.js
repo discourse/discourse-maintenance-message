f348aafe0232c0938cc5a99e7abf04a23a564049
David Taylor <david@taylorhq.com>
2024-03-27 18:02:58 +0000

    DEV: Update linting

--- test/acceptance/maintenance-message-test.js
+++ test/acceptance/maintenance-message-test.js
@@ -1,6 +1,6 @@
-import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 import { visit } from "@ember/test-helpers";
 import { test } from "qunit";
+import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 
 acceptance("Maintenance Message - Anonymous", function () {
   test("Viewing Topic", async function (assert) {
